5af8ee07d7f3902846873522a8a4b7daf71cadc3
thomas <tom.angotti11@gmail.com>
2023-08-17 17:41:17 -0600

    added create room front end

--- api/views.py
+++ api/views.py
@@ -28,8 +28,10 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_so_skip = votes_so_skip
                 room.save(update_fields=['guest_can_pause', 'votes_so_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_so_skip=votes_so_skip)
                 room.save()
-
-            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+            
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,100 @@
 import React, {Component} from "react";
+import Button from "@material-ui/core/Button";
+import  Grid  from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "@material-ui/core";
+import { FormHelperText } from "@material-ui/core";
+import { FormControl } from "@material-ui/core";
+import {Link} from "react-router-dom";
+import { Radio } from "@material-ui/core";
+import { RadioGroup } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+
+
     constructor(props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        }
+
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+    }
+
+    handleVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value
+        });
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            guestCanPause: e.target.value === "true" ? true : false,
+        })
+    }
+
+    handleRoomButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                votes_so_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause,
+            })
+        }
+        fetch('/api/create_room', requestOptions)
+            .then((res) => res.json())
+            .then((data) => console.log(data))
     }
 
     render() {
-        return <p>this is the create room page</p>
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography Component='h4' variant='h4'>
+                        Create A room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl component="fieldset" >
+                        <FormHelperText>
+                            <div align="center">
+                                Guest Control of Playback state
+                            </div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue='true' onChange={this.handleGuestCanPauseChange}>
+                            <FormControlLabel value="true" control={<Radio color="primary" />} label="Play/Pause" labelPlacement="bottom"/>
+                            <FormControlLabel value="false" control={<Radio color="secondary" />} label="No control" labelPlacement="bottom"/>
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField required={true} type="number" onChange={this.handleVotesChange} defaultValue={this.defaultVotes} inputProps={
+                            {
+                                min: 1, 
+                                style: {
+                                    textAlign: "center"
+                                    }}} />
+                        <FormHelperText>
+                            <div align="center">
+                                Votes Required To Skip Song
+                            </div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}>Create A Room</Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
+                </Grid>
+            </Grid>)
     }
 }
