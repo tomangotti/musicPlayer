9d82790f083ae797a25ca966a4d0fafc810ab7c4
thomas <tom.angotti11@gmail.com>
2023-08-17 16:35:32 -0600

    added create room backend

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create_room', CreateRoomView.as_view())
 ]
